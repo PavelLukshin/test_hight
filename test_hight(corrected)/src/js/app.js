769f4718108ee38f3e23a813e7e7460846b08502
PavelLukshin <46348989+PavelLukshin@users.noreply.github.com>
2019-04-17 12:26:56 +0300

    correction

--- test_hight(corrected)/src/js/app.js
+++ test_hight(corrected)/src/js/app.js
@@ -38,10 +38,10 @@
         for (let i = 0; i < data.items.length; i++) {
           const item = $(`<div class="item-${i+1}">
             <img src="assets/images/icon-${i+1}.png" alt="icon-${i+1}">
-            <div class="hover">
-              <div class="hover-img">
-                  <a href="#"><img src="assets/images/eye.png" alt="eye"></a>
-                  <a href="#"><img src="assets/images/heart.png" alt="heart"></a>
+            <div class="change">
+              <div class="change-img">
+                  <a href="#"><img src="assets/images/icon-eye.png" alt="eye"></a>
+                  <a href="#"><img src="assets/images/icon-heart.png" alt="heart"></a>
               </div>
               <p>${data.items[i].title}<br><span>${data.items[i].description}</span></p>
             </div>
